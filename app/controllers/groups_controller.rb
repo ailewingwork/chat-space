bc7f8e09559c07babfb0731bcf2ccfd57547fe6d
ailewingwork <ailewingwork@gmail.com>
2020-03-18 17:49:29 +0900

    unicorn install

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,7 +17,9 @@ class GroupsController < ApplicationController
     else
       render :new
     end
-    # レビューでupdate 削除？？
+  end
+
+  def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
